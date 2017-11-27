82687e5d60f7f2ca7d71f9337d7fcc37a0484525
GuillaumeDgr <dangerguillaume@gmail.com>
2017-11-27 11:32:03 +0100

    liyfktd

--- app/src/main/java/fr/wildcodeschool/atelierdesignpatterns/NewsSingleton.java
+++ app/src/main/java/fr/wildcodeschool/atelierdesignpatterns/NewsSingleton.java
@@ -1,5 +1,11 @@
 package fr.wildcodeschool.atelierdesignpatterns;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Observable;
@@ -22,6 +28,22 @@ class NewsSingleton extends Observable {
 
     void loadNews() {
         // TODO : load news from Firebase then notifiy observers
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference newsRef = database.getReference();
+        newsRef.child("news").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot dsp : dataSnapshot.getChildren()) {
+                    NewsModel newsModel = dsp.getValue(NewsModel.class);
+                    mNewsList.add(newsModel);
+                }
+                setChanged();
+                notifyObservers();
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
     }
 
     List<NewsModel> getNews() {
